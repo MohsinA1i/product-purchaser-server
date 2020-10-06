a7231fb908f582ea88f6ae050eee41f1b546f4b3
Mohsin Ali <mohsin.ali.the.great@gmail.com>
2020-10-06 22:02:11 +0500

    Renaming

--- function-manager.js
+++ function-manager.js
@@ -5,15 +5,15 @@ const Lambda = new Aws.Lambda({ httpOptions: { timeout: 180000 } });
 
 class FunctionManager {
     invoke(name, data) {
-        const token = Uuid.v4();
+        const functionId = Uuid.v4();
         const params = {
             FunctionName: name, 
-            Payload: JSON.stringify({ body: data, functionId: token }),
+            Payload: JSON.stringify({ body: data, functionId: functionId }),
         };
         Lambda.invoke(params, function(error, data) {
             if (error) console.log(error, error.stack);
         });
-        return token;
+        return functionId;
     }
 }
 
--- server.js
+++ server.js
@@ -3,71 +3,35 @@ const WebSocket = require('ws');
 const Server = Http.createServer();
 const WebSocketServer = new WebSocket.Server({ noServer: true });
 
-const FunctionManager = require('./function-manager.js');
+const FunctionManager = require('./function-manager');
 const functionManager = new FunctionManager();
 
-const awaitingClients = {};
-const type = {
+const connectionType = {
     CLIENT: 0,
     FUNCTION: 1
 }
 
-WebSocketServer.on('connection', function connection(connection) {
-    connection.socket.on('message', function incoming(message) {
-        if (connection.type === type.FUNCTION) {
-            connection.client.socket.send(message);
-        } else if (connection.type === type.CLIENT) {
-            if (connection.function) {
-                connection.function.socket.send(message);
-            } else {
-                try {
-                    const request = JSON.parse(message);
-                    if (request.function === 'task') {
-                        const token = functionManager.invoke('product-purchaser-TaskFunction-432SLL7MB6I4', request.data);
-                        awaitingClients[token] = connection;
-                        console.log(`Invoked function ${request.function} with token ${token}`);
-                    } else if (request.function === 'test') {
-                        const token = functionManager.invoke('product-purchaser-TestFunction-CP6ST80NZ07R', request.data);
-                        awaitingClients[token] = connection;
-                        console.log(`Invoked function ${request.function} with token ${token}`);
-					}	
-                } catch (error) { console.log(error.message) }
-            }
-        }
-    });
-
-    connection.socket.on('close', function close(code, reason) {
-        if (code >= 1004 || code <= 1006) code = 1001;
-        if (connection.type === type.FUNCTION) {
-            if (connection.client && connection.client.socket.readyState === WebSocket.OPEN)
-                connection.client.socket.close(code, reason);
-        } else if (connection.type === type.CLIENT) {
-            if (connection.function && connection.function.socket.readyState === WebSocket.OPEN)
-                connection.function.socket.close(code, reason);
-        }
-        delete connection;
-    });
-});
+const awaitingClients = {};
 
 Server.on('upgrade', function upgrade(request, socket, head) {
     const connection = {};
 
-    const token = request.headers['function'];
-    if (token) {
-        const client = awaitingClients[token];
-        delete awaitingClients[token];
+    const functionId = request.headers['function'];
+    if (functionId) {
+        const client = awaitingClients[functionId];
+        delete awaitingClients[functionId];
         if (client === undefined || client.readyState === WebSocket.CLOSING || client.readyState === WebSocket.CLOSED) {
             socket.destroy();
-            console.log(`No client for token ${token}`);
+            console.log(`No client for functionId ${functionId}`);
             return;
         } else {
-            connection.type = type.FUNCTION;
+            connection.type = connectionType.FUNCTION;
             connection.client = client;
             client.function = connection;
-            console.log(`Function connected with token ${token}`);
+            console.log(`Function connected with functionId ${functionId}`);
         }    
     } else {
-        connection.type = type.CLIENT;
+        connection.type = connectionType.CLIENT;
         console.log(`Client connected`);
     }
 
@@ -76,6 +40,42 @@ Server.on('upgrade', function upgrade(request, socket, head) {
         WebSocketServer.emit('connection', connection);
     });
 });
-  
+
+WebSocketServer.on('connection', function connection(connection) {
+    connection.socket.on('message', function incoming(message) {
+        if (connection.type === connectionType.FUNCTION) {
+            connection.client.socket.send(message);
+        } else if (connection.type === connectionType.CLIENT) {
+            if (connection.function) {
+                connection.function.socket.send(message);
+            } else {
+                try {
+                    const request = JSON.parse(message);
+                    if (request.function === 'task') {
+                        const functionId = functionManager.invoke('product-purchaser-TaskFunction-432SLL7MB6I4', request.data);
+                        awaitingClients[functionId] = connection;
+                        console.log(`Invoked function ${request.function} with functionId ${functionId}`);
+                    } else if (request.function === 'test') {
+                        const token = functionManager.invoke('product-purchaser-TestFunction-CP6ST80NZ07R', request.data);
+                        awaitingClients[token] = connection;
+                        console.log(`Invoked function ${request.function} with functionId ${token}`);
+					}	
+                } catch (error) { console.log(error.message) }
+            }
+        }
+    });
+
+    connection.socket.on('close', function close(code, reason) {
+        if (code >= 1004 || code <= 1006) code = 1001;
+        if (connection.type === connectionType.FUNCTION) {
+            if (connection.client && connection.client.socket.readyState === WebSocket.OPEN)
+                connection.client.socket.close(code, reason);
+        } else if (connection.type === connectionType.CLIENT) {
+            if (connection.function && connection.function.socket.readyState === WebSocket.OPEN)
+                connection.function.socket.close(code, reason);
+        }
+        delete connection;
+    });
+});
 
 Server.listen(8080);
